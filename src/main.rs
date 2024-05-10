d8250965971586f755f4a269b732025dfa6175d9
lucasmolinari <lucas.molinari2003@gmail.com>
2024-05-10 15:29:10 -0300

    Start XML parsing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use std::env; 
 use std::io;
 use std::fs;
+use std::io::Read;
 use std::path::{Path, PathBuf};
 use std::ffi::OsStr;
+
+use zip::read::ZipArchive;
+use quick_xml::events::Event;
+use quick_xml::reader::Reader;
 
 type Result<T> = io::Result<T>;
 
@@ -12,6 +17,7 @@ fn main() {
         eprintln!("Expected 1 argument but {} were supplied.", args.len() - 1);
         std::process::exit(0);
     }
+
     let source_path = match Path::new(args.get(1).unwrap()).canonicalize() {
         Ok(p) => {
             if p.extension().and_then(OsStr::to_str) != Some("xlsx") {
@@ -25,6 +31,7 @@ fn main() {
             std::process::exit(0);
         }
     };
+
     let file = match create_temp(&source_path) {
         Ok(f) => f,
         Err(e) => {
@@ -32,8 +39,7 @@ fn main() {
             std::process::exit(0);
         }
     };
-    let contents = fs::read_to_string(file).unwrap();
-    dbg!(contents);
+    read_zipped(&file).unwrap();
 }
 
 fn create_temp(src_path: &PathBuf) -> Result<tempfile::NamedTempFile> {
@@ -43,3 +49,37 @@ fn create_temp(src_path: &PathBuf) -> Result<tempfile::NamedTempFile> {
     Ok(tmpf)
 }
 
+fn read_zipped(file: &tempfile::NamedTempFile) -> Result<()> {
+    let mut arch = ZipArchive::new(file).expect("Valid Zip File");
+    
+    for i in 0..arch.len() {
+        let mut file = arch.by_index(i)?;
+
+        if file.name().starts_with("xl/worksheets/sheet") {
+            println!("{}", file.name());
+            let mut xml = String::new();
+            file.read_to_string(&mut xml)?;
+            unlock(&xml)?;
+        }
+    }
+    Ok(())
+}
+
+fn unlock(xml: &str) -> Result<()> {
+    let mut reader = Reader::from_str(xml);
+
+    let mut buf = Vec::new();
+    loop {
+        match reader.read_event_into(&mut buf){
+            Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
+            Ok(Event::Eof) => break,
+            Ok(Event::Start(ref e)) => println!("Found Start: {:?}", String::from_utf8_lossy(e)),
+            Ok(Event::End(ref e)) => println!("Found End: {:?}", String::from_utf8_lossy(e)),
+            Ok(Event::Text(ref e)) => {
+            println!("Found: {:?}", String::from_utf8_lossy(e));
+        }
+        _ => {}
+        };
+    }
+    Ok(())
+}
