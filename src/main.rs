45b634737b43d1cadac7c5f7d3db01f6b2535115
lucasmolinari <lucas.molinari2003@gmail.com>
2024-05-12 18:31:43 -0300

    Deus me proteja

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,52 @@
-use std::env; 
-use std::io;
-use std::fs;
-use std::io::Read;
-use std::path::{Path, PathBuf};
+use std::env;
 use std::ffi::OsStr;
+use std::fs;
+use std::io;
+use std::io::Write;
+use std::io::{Cursor, Read};
+use std::path::{Path, PathBuf};
 
-use zip::read::ZipArchive;
 use quick_xml::events::Event;
 use quick_xml::reader::Reader;
+use quick_xml::Writer;
+use zip::read::ZipArchive;
+use zip::write::SimpleFileOptions;
+use zip::ZipWriter;
 
 type Result<T> = io::Result<T>;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        eprintln!("Expected 1 argument but {} were supplied.", args.len() - 1);
+
+    if args.len() != 3 {
+        eprintln!("Expected 2 arguments but {} were supplied.", args.len() - 1);
+        println!("Usage: -- <source path> <destination path>");
         std::process::exit(0);
     }
 
-    let source_path = match Path::new(args.get(1).unwrap()).canonicalize() {
+    let src_path = match Path::new(args.get(1).unwrap()).canonicalize() {
         Ok(p) => {
             if p.extension().and_then(OsStr::to_str) != Some("xlsx") {
                 eprintln!("File extension should be xlsx.");
                 std::process::exit(0);
             }
             p
-        },
+        }
         Err(_) => {
             eprintln!("Couldn't find the specified file.");
             std::process::exit(0);
         }
     };
 
-    let file = match create_temp(&source_path) {
-        Ok(f) => f,
-        Err(e) => {
-            eprintln!("Couldn't create temporary file: {}", e);
+    let dest_path = match Path::new(args.get(2).unwrap()).canonicalize() {
+        Ok(p) => p,
+        Err(_) => {
+            eprintln!("Couldn't find destination path.");
             std::process::exit(0);
         }
     };
-    read_zipped(&file).unwrap();
+
+    read_zipped(&src_path, &dest_path).unwrap();
 }
 
 fn create_temp(src_path: &PathBuf) -> Result<tempfile::NamedTempFile> {
@@ -49,37 +56,55 @@ fn create_temp(src_path: &PathBuf) -> Result<tempfile::NamedTempFile> {
     Ok(tmpf)
 }
 
-fn read_zipped(file: &tempfile::NamedTempFile) -> Result<()> {
-    let mut arch = ZipArchive::new(file).expect("Valid Zip File");
-    
+fn read_zipped(src: &PathBuf, dest: &PathBuf) -> Result<()> {
+    let tsrc = create_temp(src)?;
+    let mut arch = ZipArchive::new(tsrc)?;
+
+    let rdest = fs::File::create(dest)?;
+    let mut warch = ZipWriter::new(rdest);
+
     for i in 0..arch.len() {
         let mut file = arch.by_index(i)?;
+
+        let options =
+            SimpleFileOptions::default().compression_method(zip::CompressionMethod::Stored);
+        warch.start_file(file.name(), options)?;
 
         if file.name().starts_with("xl/worksheets/sheet") {
             println!("{}", file.name());
             let mut xml = String::new();
             file.read_to_string(&mut xml)?;
-            unlock(&xml)?;
+
+            let unlocked = unlock(&xml)?;
+            warch.write(unlocked.as_bytes())?;
+            continue;
         }
     }
     Ok(())
 }
 
-fn unlock(xml: &str) -> Result<()> {
+fn unlock(xml: &str) -> Result<String> {
     let mut reader = Reader::from_str(xml);
+    let mut writer = Writer::new(Cursor::new(Vec::new()));
 
     let mut buf = Vec::new();
     loop {
-        match reader.read_event_into(&mut buf){
+        match reader.read_event_into(&mut buf) {
             Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
             Ok(Event::Eof) => break,
-            Ok(Event::Start(ref e)) => println!("Found Start: {:?}", String::from_utf8_lossy(e)),
-            Ok(Event::End(ref e)) => println!("Found End: {:?}", String::from_utf8_lossy(e)),
-            Ok(Event::Text(ref e)) => {
-            println!("Found: {:?}", String::from_utf8_lossy(e));
-        }
-        _ => {}
+            Ok(Event::Empty(e)) => {
+                if e.name().as_ref() == b"sheetProtection" {
+                    continue;
+                }
+            }
+            Ok(e) => {
+                writer.write_event(e).unwrap();
+            }
         };
     }
-    Ok(())
+    let r = String::from_utf8(writer.into_inner().into_inner());
+    match r {
+        Ok(s) => Ok(s),
+        Err(e) => Err(io::Error::new(io::ErrorKind::InvalidData, e)),
+    }
 }
