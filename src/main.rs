4459ca96b88611b070f8bb7bcad97897058d8fcc
lucasmolinari <lucas.molinari2003@gmail.com>
2024-05-14 10:00:48 -0300

    Optimize XML parsing

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::io;
 use std::io::Write;
 use std::io::{Cursor, Read};
 use std::path::{Path, PathBuf};
+use std::time::Instant;
 
 use quick_xml::events::Event;
 use quick_xml::reader::Reader;
@@ -28,6 +29,7 @@ fn main() {
         Ok(p) => {
             let ext = p.extension().and_then(OsStr::to_str);
             if ext != Some("xlsx") && ext != Some("xlsm") {
+                eprintln!("Supported extensions are: xlsx and xlsm");
                 std::process::exit(0);
             }
             p
@@ -46,7 +48,7 @@ fn main() {
 
     match unlock(&src_path, &dest_path) {
         Ok(_) => println!("Process Finished."),
-        Err(e) => eprintln!("{}", e),
+        Err(e) => eprintln!("Process Finished with error: {}", e),
     };
 }
 
@@ -58,7 +60,8 @@ fn create_temp(src_path: &PathBuf) -> Result<tempfile::NamedTempFile> {
 }
 
 fn unlock(src: &PathBuf, dest: &PathBuf) -> Result<()> {
-    let tsrc = create_temp(src)?;
+    let time = Instant::now();
+    let tsrc = create_temp(src).unwrap();
     let mut arch = ZipArchive::new(tsrc)?;
 
     let rdest = fs::File::create(dest)?;
@@ -75,7 +78,7 @@ fn unlock(src: &PathBuf, dest: &PathBuf) -> Result<()> {
         match file.read_to_end(&mut content) {
             Ok(_) => {}
             Err(e) => {
-                println!("Received error trying to read [{}]: {}", file.name(), e);
+                eprintln!("Received error trying to read [{}]: {}", file.name(), e);
                 println!("File skipped.");
                 continue;
             }
@@ -85,37 +88,56 @@ fn unlock(src: &PathBuf, dest: &PathBuf) -> Result<()> {
                 .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
 
             let unlocked = remove_tag(&cstr, "sheetProtection")?;
-            warch.write(unlocked.as_bytes())?;
+            warch.write(unlocked.as_ref())?;
 
             continue;
         }
+        if file.name().starts_with("xl/workbook.xml") {
+            let cstr = std::str::from_utf8(&content)
+                .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))
+                .unwrap();
+
+            let unlocked = remove_tag(&cstr, "workbookProtection")?;
+            warch.write(unlocked.as_ref())?;
+
+            continue;
+        }
+
         warch.write_all(&content)?;
     }
+    println!("{:?}", time.elapsed());
     Ok(())
 }
 
-fn remove_tag(xml: &str, tag: &str) -> Result<String> {
+fn remove_tag(xml: &str, tag: &str) -> Result<Vec<u8>> {
     let mut reader = Reader::from_str(xml);
     let mut writer = Writer::new(Cursor::new(Vec::new()));
 
     let mut buf = Vec::new();
     loop {
         match reader.read_event_into(&mut buf) {
-            Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
+            Err(e) => eprintln!("Error at position {}: {:?}", reader.buffer_position(), e),
             Ok(Event::Eof) => break,
             Ok(Event::Empty(e)) => {
                 if e.name().as_ref() != tag.as_bytes() {
-                    writer.write_event(Event::Empty(e)).unwrap();
+                    writer
+                        .write_event(Event::Empty(e))
+                        .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
                 }
             }
+            Ok(Event::Start(e)) => {
+                let end = e.to_end();
+                reader
+                    .read_to_end(end.name())
+                    .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
+            }
             Ok(e) => {
-                writer.write_event(e).unwrap();
+                writer
+                    .write_event(e)
+                    .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
             }
         };
     }
 
-    match String::from_utf8(writer.into_inner().into_inner()) {
-        Ok(s) => Ok(s),
-        Err(e) => Err(io::Error::new(io::ErrorKind::InvalidData, e)),
-    }
+    Ok(writer.into_inner().into_inner())
 }
